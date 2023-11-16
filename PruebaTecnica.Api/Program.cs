278faa31e155b89ab32150fc14a1eafbfb5f4cfe
YeanOrma <germ.ortiz@udla.edu.co>
2023-11-16 13:49:01 -0500

    Inyección de dependencia y configuración del Context

--- PruebaTecnica.Api/Program.cs
+++ PruebaTecnica.Api/Program.cs
@@ -1,3 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using PruebaTecnica.Application.Interfaces;
+using PruebaTecnica.Application.Services;
+using PruebaTecnica.Data.Context;
+using PruebaTecnica.Data.Repository;
+using PruebaTecnica.Domain.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<LabDevContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("sqlServerConnection")));
+
+builder.Services.AddScoped<IClienteService, ClienteService>();
+builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
 
 var app = builder.Build();
 
