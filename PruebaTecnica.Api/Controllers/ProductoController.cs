7703dc30b5a494690b17ca10da518b08f7fb3518
YeanOrma <germ.ortiz@udla.edu.co>
2023-11-16 14:33:40 -0500

    Creando el endPoint para obtener los productos

---
+++ PruebaTecnica.Api/Controllers/ProductoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PruebaTecnica.Application.Interfaces;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PruebaTecnica.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        private readonly IProductoService productoService;
+
+        public ProductoController(IProductoService productoService)
+        {
+            this.productoService = productoService;
+        }
+
+        // GET: api/<ProductoController>
+        [HttpGet("obtenerProductos")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                return Ok(await productoService.GetProductos());
+            }catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
