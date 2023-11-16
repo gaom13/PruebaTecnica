38d86f47c4fc49612a11d9dc206399e13a417c87
YeanOrma <germ.ortiz@udla.edu.co>
2023-11-16 13:48:14 -0500

    Creando el endPoint para los Clientes

---
+++ PruebaTecnica.Api/Controllers/ClienteController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PruebaTecnica.Application.Interfaces;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PruebaTecnica.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly IClienteService clienteService;
+
+        public ClienteController(IClienteService clienteService)
+        {
+            this.clienteService = clienteService;
+        }
+
+        // GET: api/<ClienteController>
+        [HttpGet("obtenerClientes")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                return Ok(await clienteService.GetClientes());
+            }catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
