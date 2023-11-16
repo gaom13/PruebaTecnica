47a6a8759038f74ba643531be68000c55b97b988
YeanOrma <germ.ortiz@udla.edu.co>
2023-11-16 14:19:57 -0500

    Creando los DTOs para obtener los clientes

---
+++ PruebaTecnica.Domain/DTOs/CatTipoClienteDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaTecnica.Domain.DTOs
+{
+    public class CatTipoClienteDTO
+    {
+        public string TipoCliente { get; set; } = null!;
+    }
+}
---
+++ PruebaTecnica.Domain/DTOs/TblClienteDTO.cs
@@ -0,0 +1,22 @@
+﻿using PruebaTecnica.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaTecnica.Domain.DTOs
+{
+    public class TblClienteDTO
+    {
+        public int Id { get; set; }
+
+        public string RazonSocial { get; set; } = null!;
+
+        public int IdTipoCliente { get; set; }
+
+        public string Rfc { get; set; } = null!;
+
+        public virtual CatTipoClienteDTO IdTipoClienteNavigation { get; set; } = null!;
+    }
+}
